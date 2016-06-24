f7e152145477d9d9bdb5efdb6139258e1b5c8c34
zhaoName <zhao1529835@126.com>
2016-06-24 16:24:25 +0800

    Initial Commit

---
+++ ScrollLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollLabel
+//
+//  Created by zhao on 16/6/24.
+//  Copyright © 2016年 zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
